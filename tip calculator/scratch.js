b42b6470f1630b4bf8f67f88c267bc79ccdcda10
ZeldaHatGuy <Equin0xx@yahoo.com>
2022-12-13 01:31:05 -0600

    removing scratch file

--- tip calculator/scratch.js
+++
@@ -1,8 +0,0 @@
- let data = [
-    {tipPercentage: "15%", bill: "", tipAmount: "", totalBill:""},
-    {tipPercentage: "20%", bill: "", tipAmount: "", totalBill:""},
-    {tipPercentage: "25%", bill: "", tipAmount: "", totalBill:""},
-    {tipPercentage: "", bill: "", tipAmount: "", totalBill:""}
- ]
-
- console.log(data[0].tipPercentage)
